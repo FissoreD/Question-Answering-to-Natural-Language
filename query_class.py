d47cbd4f736c7360582ac6055cc95c21f675c15f
Antoine Venturelli <ant.venturelli06@gmail.com>
2021-12-14 18:16:02 +0100

    Possibilité de répondre à é questions dans une requête + Racinisation des mots pour avoir + de résultat pertinent
    Exemple: la date de naissance et le lieu de naissance du président de la France
    Co-Authored-By: FissoreD <fisso9900@gmail.com>

--- query_class.py
+++ query_class.py
@@ -48,10 +48,11 @@ class query:
 
     def __init__(self, domain: str, attribute, father="", proba=1,
                  model=read_input.read_model()) -> None:
-        self.domain = domain.replace('"', "")
+        self.domain = domain.replace('"', "").replace("-", "_")
         self.father = father
         self.proba = proba
-        self.attribute = attribute.lower()
+        print("attribute : ", attribute)
+        self.attribute = attribute.lower().replace("-", "")
         self.model = model
         self.sparql = self.create_sparql()
         self.link = self.create_link()
@@ -144,9 +145,9 @@ class main:
         self.model = model
         self.domain = w1.pop().capitalize()
         first_attribute = w1.pop()
-        self.res = {self.domain: query(
-            self.domain, first_attribute, model=model, father=f"-> {first_attribute}")}
-        self.res[self.domain].initiate()
+        self.make_query(
+            self.domain, f"-> {first_attribute}", first_attribute, 1, [], first_attribute)
+
         # print(self.res[-1])
         current_lvl: list = []
         next_lvl: list = []
@@ -155,38 +156,59 @@ class main:
             current_attribute = w1.pop()
             while current_lvl:
                 current_object, _, father = current_lvl.pop(0)
-                for i in current_object.best_match[:5]:
-                    current_domain_list = i['res']
-                    current_proba = i['probability']
-                    # TODO
-                    current_father = f"{father} -> {i['value']}"
-                    # Si la liste liée au domain courant à plus
-                    # de 5 élément alors on le considère pas
-                    if len(current_domain_list) > 5:
-                        continue
-                    for current_domain in current_domain_list:
-                        # si le domain courant est un nombre on skip
-                        try:
-                            float(current_domain)
+                print(current_object)
+                for k in current_object:
+                    for i in k.best_match[:5]:
+                        current_domain_list = i['res']
+                        current_proba = i['probability']
+                        # TODO
+                        current_father = f"{father} -> {i['value']}"
+                        # Si la liste liée au domain courant à plus
+                        # de 5 élément alors on le considère pas
+                        if len(current_domain_list) > 5:
                             continue
-                        except ValueError:
-                            pass
-                        print(i['value'], current_domain, current_attribute)
-                        o1 = query(current_domain.replace(' ', '_'),
-                                   current_attribute, father=f"{current_father} ({first_attribute}) -> {removeHTTP(current_domain)}", model=self.model, proba=current_proba)
-                        o1.initiate()
-                        if o1.best_match != []:
-                            self.res[removeHTTP(current_domain)] = o1
-                        next_lvl.append(
-                            (o1, current_proba, f"{current_father} ({first_attribute}) -> {removeHTTP(current_domain)}"))
+                        for current_domain in current_domain_list:
+                            # si le domain courant est un nombre on skip
+                            try:
+                                float(current_domain)
+                                continue
+                            except ValueError:
+                                pass
+                            print(i['value'], current_domain,
+                                  current_attribute)
+                            self.make_query(current_domain, current_father, first_attribute,
+                                            current_proba, next_lvl, current_attribute)
+
             first_attribute = current_attribute
 
             while next_lvl:
                 current_lvl.append(next_lvl.pop(0))
         self.last_lvl = current_lvl
 
+    def make_query(self, current_domain, current_father, first_attribute, current_proba, next_lvl, current_attribute):
+        def send_query(s):
+            o1 = query(current_domain.replace(' ', '_'),
+                       s, father=f"{current_father} ({first_attribute}) -> {removeHTTP(current_domain)}", model=self.model, proba=current_proba)
+            o1.initiate()
+            if o1.best_match != []:
+                try:
+                    self.res[current_domain].append(o1)
+                except KeyError:
+                    self.res[current_domain] = [o1]
+                except AttributeError:
+                    self.res = {self.domain: [o1]}
+                next_lvl.append(
+                    ([o1], current_proba, f"{current_father} ({first_attribute}) -> {removeHTTP(current_domain)}"))
+
+        print(type(current_attribute))
+        if type(current_attribute) == list:
+            for s in current_attribute:
+                send_query(s)
+        else:
+            send_query(current_attribute)
+
     def get_result(self):
-        return {i: self.res[i].dict_without_float() for i in self.res}
+        return {i: [k.dict_without_float() for k in self.res[i]] for i in self.res}
 
     def get_last_lvl(self):
         return [i[0].dict_without_float() for i in self.last_lvl]
@@ -194,24 +216,35 @@ class main:
     def flat_list(self):
         L = []
         for x, y, z in self.last_lvl:
-            L.extend(x.dict_without_float()[1])
+            [L.extend(i) for i in [k.dict_without_float()[1] for k in x]]
         print(L[:2])
         L.sort(key=lambda x: float(x['probability']))
         return L
 
+    def pretty_print(self, verbose=False):
+        L = self.flat_list()
+        [print("Probabilité : {} % - Résultat : {}".format(int(float(e["probability"]) * 100), [removeHTTP(i) for i in e["res"]]), "- Father : {}".format(e["father"]) if verbose else "")
+         for e in L[-10:]]
+
 
 if __name__ == '__main__':
+    questions = ["What is the children, birthdate and birthplace of the major of the capital of France ?",
+                 "What is the birthdate and birthplace of foundator of Microsoft ?",
+                 "What is the name of the president of the country with Venice ?",
+                 "What is the date of Valentine's-Day",
+                 "What is the birthplace and the birthdate of Emmanuel_Macron ?"]
+
     print('Charging')
     t = time.time()
     model = read_input.read_model()
-    inp = read_input.parse_sentence(
-        "What is the president of upper-house of France")
+    inp = read_input.main(questions[1])
     print(inp)
     m = main(inp, model)
     with open('./output/' + m.domain + '.json', 'w') as fp:
         json.dump(m.get_result(), fp)
     # print(json.dumps(m.get_last_lvl(), indent=2))
     print(json.dumps(m.flat_list(), indent=2))
+    m.pretty_print()
     print(time.time() - t)
 
 """
@@ -223,11 +256,4 @@ todo :
     les deux :
         ex : what is the birthdate and the birthplace of the president of USA ?
 
-"""
-# %%
-
-"""
- What is the birthplace of the major of the capital of the country with Paris ?    
-
- What is the name of the president of the country with Venice ?
 """
--- read_input.py
+++ read_input.py
@@ -2,6 +2,8 @@ from nltk.corpus import brown
 import gensim
 import os
 import functools
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
 # from PyDictionary import PyDictionary
 from bs4 import BeautifulSoup
 import requests
@@ -27,7 +29,8 @@ def read_model():
 def similarity(model, wordA, wordB):
     wordBs = wordB.split()
     if len(wordBs) == 1:
-        if wordA == wordB:
+        lancaster = LancasterStemmer()
+        if wordA == wordB or lancaster.stem(wordA) == lancaster.stem(wordB):
             return 1
         try:
             return model.wv.similarity(wordA, wordBs[0])
@@ -65,29 +68,50 @@ def clean_input(str):
 
 
 accepted_classes = ["noun", "abbreviation"]
+friendly_list = [chr(ord('a') + i) for i in range(26)] + \
+    [chr(ord('A') + i) for i in range(26)] + list("_-'")
 
 
-def parse_sentence(str):
+def parse_sentence(question):
     res = []
-    words = str.lower().strip().split()
+
+    words = question.lower().strip().split()
     for w in words:
+        [w := w.replace(i, "") for i in list(w) if i not in friendly_list]
+        if w == "":
+            continue
         r = requests.get("https://www.dictionary.com/browse/" +
-                         w + "#", allow_redirects=True)
+                         w.replace("'", "-") + "#", allow_redirects=True)
         soup = BeautifulSoup(r.text, 'html.parser')
         cla = soup.find('span', {'class': 'luna-pos'})
         if cla == None:
+            res.append(w)
             continue
         word_class = cla.text.strip()
         if word_class[-1] == ',':
             word_class = word_class[:-1]
         print((w, word_class))
         if word_class in accepted_classes:
-            res.append(w.replace('-', ''))
+            res.append(w)
         # elif word_class == 'abbreviation':
         #     # defi = soup.find('div', attrs={"value": "1"}).get_text().replace(
         #     #     ':', '.').split('.')[0]
         #     res.append(defi)
     return res
+
+
+def parse_and(question):
+    temp = question.split(" and ")
+    if len(temp) == 2:
+        first = parse_sentence(temp[0])
+        second = parse_sentence(temp[1])
+        return [first + [second[0]],  *second[1:]]
+    res = parse_sentence(temp[0])
+    return [[res[0]]] + res[1:]
+
+
+def main(question):
+    return parse_and(question)
 
 
 # def parse_sentence2():
@@ -108,10 +132,16 @@ def parse_sentence(str):
 #     return res
 
 if __name__ == '__main__':
-    print(parse_sentence(
-        "this is a sentence which only aims to confirm the ufo is a noun such as house or horse USA birthdate "))
-    # create_model()
+    print(parse_and("What is the birthplace and the birthdate of Emmanuel_Macron ?"))
+
     # m = read_model()
-    # print(similarity(m, 'world', 'world'))
+    # print(similarity(m, 'leader', 'president'))
+
+    lancaster = LancasterStemmer()
+    print(lancaster.stem("founded"))
+    print(lancaster.stem("foundator"))
+    print(lancaster.stem("troubling"))
+    print(lancaster.stem("troubled"))
+
     # print(map_similarity_ordered(map_similarity(m, 'president',
     #       ['date', 'leader', 'year', 'apple'])))
